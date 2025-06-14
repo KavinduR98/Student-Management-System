74910e9536df8101b380eae77c59d45fc152baa2
@kavinduushan07 <kavinduushan07@gmail.com>
2025-06-14 18:56:45 +0530

    impl error handler

--- src/main/java/com/ushan/studentManagementSystem/model/Student.java
+++ src/main/java/com/ushan/studentManagementSystem/model/Student.java
@@ -1,6 +1,8 @@
 package com.ushan.studentManagementSystem.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -13,10 +15,21 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotNull
     private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
     private String email;
+
+    @NotNull
     private String address;
+
+    @NotNull
     private LocalDate dateOfBirth;
+
+    @NotNull
     private LocalDate registeredDate;
 
     public UUID getId() {
